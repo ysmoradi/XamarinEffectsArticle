46b55c556ee84aff878e1f7ff9a461c596e56edc
ysmoradi <ysmoradi@outlook.com>
2017-11-14 13:40:35 +0330

    Add clear button effect to all entry elements using attached property and style

--- XamarinEffectsArticle/XamarinEffectsArticle/Effects/EntryClearButtonEffect.cs
+++ XamarinEffectsArticle/XamarinEffectsArticle/Effects/EntryClearButtonEffect.cs
@@ -4,9 +4,31 @@ namespace XamarinEffectsArticle.Effects
 {
     public class EntryClearButtonEffect : RoutingEffect
     {
-        public EntryClearButtonEffect() 
+        public EntryClearButtonEffect()
             : base("MyApp.EntryClearButtonEffect")
         {
+        }
+
+        public static readonly BindableProperty ShowClearButtonProperty = BindableProperty.CreateAttached(
+                propertyName: "ShowClearButton",
+                returnType: typeof(bool),
+                declaringType: typeof(Entry),
+                defaultValue: false,
+                propertyChanged: OnShowClearButtonChanged);
+
+        public static bool? GetShowClearButton(BindableObject view)
+        {
+            return (bool?)view.GetValue(ShowClearButtonProperty);
+        }
+
+        public static void OnShowClearButtonChanged(BindableObject view, object oldValue, object newValue)
+        {
+            view.SetValue(ShowClearButtonProperty, newValue);
+
+            Entry entry = (Entry)view;
+
+            if (((bool)newValue) == true)
+                entry.Effects.Add(new EntryClearButtonEffect());
         }
     }
 }
